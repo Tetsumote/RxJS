767d2418bddfd9f145b89f438fa068c6c47e916f
Tetsumote <unionproject88@gmail.com>
2018-04-07 00:02:09 +0200

    add observer

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,29 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx); 
+var observable = Observable.create((observer:any) => {
+    try{
+        observer.next('Hey guys!')
+        observer.next('How are you?')
+        setInterval(() => {
+            observer.next('I am good')
+        },2000)
+       // observer.complete()
+       // observer.next('This will not send')
+    } catch(err){
+        observer.error(err)
+    }
+
+});
+
+observable.subscribe(
+    (x:any) => addItem(x),
+    (error:any) => addItem(error),
+    () => addItem('Completed'),
+);
+
+function addItem(val:any) {
+    var node = document.createElement("li");
+    var textnode = document.createTextNode(val);
+    node.appendChild(textnode);
+    document.getElementById("output").appendChild(node);
+}
